84a07a30ea143feac7562da4aec782a068602282
quinso <tolegenolzas105@gmail.com>
2026-04-28 11:22:20 +0500

    xyi

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Title ('A', HitType.hit))),
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Title('A', HitType.hit)),
+      appBar: AppBar(title: Align(alignment:Alignment.centerLeft, child: Text("Bridle"))),
+      ),
     );
   }
 }
@@ -23,7 +25,21 @@ class Title extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey.shade300),
+          color: switch (hitType) {
+            HitType.hit => Colors.green,
+            HitType.miss => Colors.grey,
+            HitType.partial => Colors.yellow,
+             _=> Colors.white,
+          
+          },
+      ),
+      child: Center(child:Text(letter.toUpperCase(),))
+    );
   }
 }
-
